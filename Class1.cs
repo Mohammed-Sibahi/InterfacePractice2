5fcbc8a7ce88b88baa1de9af570b6ca1643f5098
Sibahi <sibahi@live.com>
2023-11-21 12:33:47 +0400

    create interfaces

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public interface IShape
+{
+	double CalculateArea();
+	double CalculatePerimeter();
+}
+
+public class Circle : IShape
+{
+
+}
+
+public class Triangle : IShape
+{
+
+}
+
+public class Rectangle : IShape
+{
+
+}
